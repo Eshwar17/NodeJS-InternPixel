b0bdb3225309459156e6b777ad4407ca1d31105f
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-08-23 15:56:50 +0530

    Undertood routing through routes

--- UnderstandingTheBasics/app.js
+++ UnderstandingTheBasics/app.js
@@ -1,6 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
+    const url = req.url;
+    if(url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<h1>Home Page</h1><form action="/message" method="POST"><input type="text"><button type="submit">Submit</button></form>');
+        return res.end();
+    }
     console.log(req.url, req.method);
     res.setHeader('Content-Type', 'text/html');
     res.write('<h1>Hi I am Node JS Baba</h1>');
