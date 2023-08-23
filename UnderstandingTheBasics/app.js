c22cd7e55e7bc3771654751a5de1e4790e10bce5
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-08-23 16:05:00 +0530

    Understood how to redirect and how to write write into file

--- UnderstandingTheBasics/app.js
+++ UnderstandingTheBasics/app.js
@@ -1,10 +1,18 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res) => {
     const url = req.url;
+    const method = req.method;
     if(url === '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<h1>Home Page</h1><form action="/message" method="POST"><input type="text"><button type="submit">Submit</button></form>');
+        return res.end();
+    }
+    if(url === '/message' && method === 'POST') {
+        fs.writeFileSync('messages.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
     console.log(req.url, req.method);
